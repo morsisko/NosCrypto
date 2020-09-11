73f267c82578daedd93408575c94123e8bd2e43b
morsisko <morsisko@gmail.com>
2020-09-11 23:05:07 +0200

    version change

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="noscrypto",
-    version="0.1.0",
+    version="0.1.2",
     description="A reverse engineered packet cryptography - encryption and decryption routines to emulate NosTale client or server",
     long_description=README,
     long_description_content_type="text/markdown",
